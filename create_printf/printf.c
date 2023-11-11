9e248d28eb34feae29586366c73e89969224a419
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-11-11 03:08:05 -0800

    printf

---
+++ create_printf/printf.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * _printf - returns a printf function
+ * @format:the type of data type by the user
+ * Return: number o characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int num_of_char;
+
+	va_list args;
+
+	num_of_char = 0;
+	va_start(args, format);
+	if (format == NULL)
+	{
+		exit(100);
+	}
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			num_of_char++;
+		}
+		else
+		{
+			format++;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				format++;
+				num_of_char++;
+			}
+			else if (*format == 'c')
+			{
+				char size_of_char = va_arg(args, int);
+
+				write(1, &size_of_char, 1);
+				num_of_char++;
+			}
+			else if (*format == 's')
+			{
+				char *arry = va_arg(args, char *);
+
+				int len_of_str = 0;
+
+				while (arry[len_of_str] != '\0')
+				{
+					len_of_str++;
+				}
+				write(1, arry, len_of_str);
+				num_of_char += len_of_str;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+
+	return (num_of_char);
+
+}
