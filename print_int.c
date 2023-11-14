3a2cf3dfdeee00c28a32dbabea07d90465e37e89
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-11-13 23:26:13 -0800

    updated print_int

--- print_int.c
+++ print_int.c
@@ -1,31 +1,67 @@
 #include "main.h"
+
 /**
- * print_i - prints intager
- * @args: argument to print
- * Return: intager
+ * print_i - prints integer
+ * @args: arguments to print
+ * Return: integer
  */
 
 int print_i(va_list args)
 {
-	/*declare Varianle;*/
-	int z = va_arg(args, int);
-	int number, last = z % 10, digit, expn = 1;
+	int nom = va_arg(args, int);
+	int ne_gative= 0;
+	int digit, expn = 1;
+	int jm = 0;
+
+	int temp = nom;
+
+	if (nom < 0)
+	{
+		_putchar('-');
+		ne_gative = 1;
+		nom = -nom;
+		jm++;
+	}
+	while (temp / 10 != 0)
+	{
+		expn *= 10;
+		temp /= 10;
+	}
+	while (expn >= 1)
+	{
+		digit = nom / expn;
+		_putchar(digit + '0');
+		nom %= expn;
+		expn /= 10;
+		jm++;
+	}
+	return (ne_gative ? jm + 1 : jm);
+}
+
+/**
+ * print_d - print decimal
+ * @args: arguments to be printed
+ * Return: integer
+ */
+
+int print_d(va_list args)
+{
+	int niz = va_arg(args, int);
+	int number, las = niz % 10, digit;
 	int j = 1;
+	int expn = 1;
 
-	/*extract the last digit*/
-	z = z / 10;
-	number = z;
+	niz = niz / 10;
+	number = niz;
 
-	/*check if the number is negative */
-	if (last < 0)
+	if (las < 0)
 	{
 		_putchar('-');
 		number = -number;
-		z = -z;
-		last = -last;
+		niz = -niz;
+		las = -las;
 		j++;
 	}
-
 	if (number > 0)
 	{
 		while (number / 10 != 0)
@@ -33,60 +69,17 @@ int print_i(va_list args)
 			expn = expn * 10;
 			number = number / 10;
 		}
-		number = z;
-		while (expn >= 1)
+		number = niz;
+		while (expn > 0)
 		{
 			digit = number / expn;
 			_putchar(digit + '0');
+			number = number - (digit * expn);
 			expn = expn / 10;
 			j++;
 		}
 	}
-	_putchar(last + '0');
-	return (j);
-}
+	_putchar(las + '0');
 
-/**
- * print_d - prints decimal
- * @args: argument to print
- * Return: intager
- */
-
-int print_d(va_list args)
-{
-	int z = va_arg(args, int);
-	int number, last = z % 10, digit;
-	int j = 1;
-	int expn = 1;
-
-	z = z / 10;
-	number = z;
-
-	if (last < 0)
-	{
-		_putchar('-');
-		number = -number;
-		z = -z;
-		last = -last;
-		j++;
-	}
-	if (number > 0)
-	{
-		while (number / 10 != 0)
-		{
-			expn *= 10;
-			number /= 10;
-		}
-		number = z;
-		while (expn >= 1)
-		{
-			digit = z / expn;
-			_putchar(digit + '0');
-			z %= expn;
-			expn /= 10;
-			j++;
-		}
-	}
-	_putchar(last + '0');
 	return (j);
 }
